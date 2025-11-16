580384281f755ce2d2f9bbac9de299677b8f5e51
Fohas <alexandergrigorec@mail.ru>
2025-11-16 16:35:44 +0300

    Delete module_2 directory

--- module_2/path_2/num_4.py
+++
@@ -1,8 +0,0 @@
-num1 = int(input("Enter a number: "))
-num2 = int(input("Enter another number: "))
-my_list = [num1, num2]
-min_num = min(my_list)
-max_num = max(my_list)
-if my_list == [num1 == num2]:
-    print ("The number is equal to itself")
-else: print (min_num, max_num)
